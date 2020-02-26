b43fec98dde521521d5a847f0fb5d954b9d95037
mickmed <mick2090@gmail.com>
2020-02-25 20:20:22 -0500

    mods

--- oddsFromEvens.js
+++ oddsFromEvens.js
@@ -3,27 +3,7 @@
 //input => [0,1,2,3,4,5,6,7,8,9]
 //output => [ 0, 2, 4, 6, 8, 9, 7, 5, 3, 1 ]
 const oddsFromEvens = (str) => {
-  const even = []
-  const odd = []
 
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] % 2 === 0) {
-      even.push(str[i])
-    }
-    else{
-      odd.push(str[i])
-    }
-  }
-  const sortEven = (a, b) => {
-    return a - b
-  }
-  const sortOdd = (a, b) => {
-    return b - a
-  }
-  even.sort(sortEven)
-  odd.sort(sortOdd)
-  console.log(even, odd)
-  return even.concat(odd)
 }
 
 console.log(oddsFromEvens([423, 322, 918, 45, 6, 79, 55]))
